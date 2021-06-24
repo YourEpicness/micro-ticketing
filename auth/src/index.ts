90f9223453c9df1fd0dc061f32331ce41d609803
Julaam Diop <jeebe.diop@gmail.com>
2021-06-23 21:40:34 -0400

    initial commit with some setup

---
+++ auth/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import {json} from 'body-parser';
+
+//import routers from routes folder
+import { currentUserRouter } from './routes/currentuser';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+
+const app = express();
+app.use(json());
+
+// use our imported routes
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000!!!")
+});
